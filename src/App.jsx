76f1e0a6a64fe0259616e88bccc48c526611cb40
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-07-24 06:58:10 +0530

    setup tailwind and router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Body from "./components/Body";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-3xl bg-green-50">
+        
+        <Body/>
+        
+        </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+const Login = () => {
+  return (
+    <>
+      <Header /> 
+      <div>Login</div>
+    </>
+  );
+};
+
+export default Login;
